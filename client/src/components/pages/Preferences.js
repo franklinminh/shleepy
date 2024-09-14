e4e2be4fa10d36c17fdf039fdbaabc71cbc02684
franklinminh <fmn@mit.edu>
2024-09-14 18:57:32 -0400

    pref form done

--- client/src/components/pages/Preferences.js
+++ client/src/components/pages/Preferences.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
+import { Switch, FormControlLabel, Box } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 import "./Preferences.css";
@@ -7,6 +8,16 @@ import StarsImage from "../modules/Stars.svg";
 
 const Preferences = () => {
   const navigate = useNavigate();
+
+  const [switches, setSwitches] = useState({
+    stress: false,
+    anxiety: false,
+    fatigue: false,
+  });
+
+  const handleSwitchChange = (event) => {
+    setSwitches({ ...switches, [event.target.name]: event.target.checked });
+  };
 
   return (
     <div className="background-purple">
@@ -18,7 +29,113 @@ const Preferences = () => {
           backgroundPosition: "top center",
           backgroundRepeat: "repeat-x",
         }}
-      ></div>
+      >
+        <h1 className="u-fontMontserrat u-fontSemibold u-textWhite">
+          what have you been
+          <br /> experiencing lately?
+        </h1>
+
+        {/* Switches in Column */}
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="flex-start"
+          sx={{ marginBottom: "20px" }}
+        >
+          <FormControlLabel
+            control={
+              <Switch
+                checked={switches.stress}
+                onChange={handleSwitchChange}
+                name="stress"
+                sx={{
+                  "& .MuiSwitch-switchBase.Mui-checked": {
+                    color: "var(--yellow)",
+                  },
+                  "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
+                    backgroundColor: "var(--yellow)",
+                  },
+                }}
+              />
+            }
+            label="Stress"
+            sx={{
+              fontFamily: "Montserrat, sans-serif",
+              color: switches.stress ? "var(--yellow)" : "white",
+              fontWeight: "bold",
+              marginBottom: "10px",
+            }}
+          />
+          <FormControlLabel
+            control={
+              <Switch
+                checked={switches.anxiety}
+                onChange={handleSwitchChange}
+                name="anxiety"
+                sx={{
+                  "& .MuiSwitch-switchBase.Mui-checked": {
+                    color: "var(--yellow)",
+                  },
+                  "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
+                    backgroundColor: "var(--yellow)",
+                  },
+                }}
+              />
+            }
+            label="Anxiety/Nervousness"
+            sx={{
+              fontFamily: "Montserrat, sans-serif",
+              color: switches.anxiety ? "var(--yellow)" : "white",
+              fontWeight: "bold",
+              marginBottom: "10px",
+            }}
+          />
+          <FormControlLabel
+            control={
+              <Switch
+                checked={switches.fatigue}
+                onChange={handleSwitchChange}
+                name="fatigue"
+                sx={{
+                  "& .MuiSwitch-switchBase.Mui-checked": {
+                    color: "var(--yellow)",
+                  },
+                  "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
+                    backgroundColor: "var(--yellow)",
+                  },
+                }}
+              />
+            }
+            label="Fatigue"
+            sx={{
+              fontFamily: "Montserrat, sans-serif",
+              color: switches.fatigue ? "var(--yellow)" : "white",
+              fontWeight: "bold",
+              marginBottom: "10px",
+            }}
+          />
+        </Box>
+
+        <Button
+          variant="contained"
+          sx={{
+            fontFamily: "Montserrat, sans-serif",
+            backgroundColor: "white",
+            color: "var(--purple)",
+            fontWeight: "bold",
+            textTransform: "none",
+            "&:hover": {
+              backgroundColor: "#e0e0e0",
+            },
+          }}
+          onClick={() => {
+            navigate("/shleep", { state: { switches } });
+          }}
+        >
+          Generate Custom Playlist
+        </Button>
+      </div>
     </div>
   );
 };
